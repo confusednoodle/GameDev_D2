5f924da8969ec30382057fffad32d70354bada9e
confusednoodle <h8pdki@protonmail.com>
2023-07-15 22:22:50 +0200

    added sound to fleshlight script & assigned it in world 3

--- Assets/Scripts/Player/FlashlightController.cs
+++ Assets/Scripts/Player/FlashlightController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class FlashlightController : MonoBehaviour
 {
     [SerializeField] Light Light;
+    [SerializeField] AudioSource fleshlight;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("f"))
         {
+            fleshlight.Play();
             Light.enabled = !Light.enabled;
         }
     }
