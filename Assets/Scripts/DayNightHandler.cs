83ae0c4b40f79ffcef4cdc176561345f19614f60
miampf <miampf@proton.me>
2023-07-15 16:53:30 +0200

    made night and day handler work in w1 and w2

--- Assets/Scripts/DayNightHandler.cs
+++ Assets/Scripts/DayNightHandler.cs
@@ -6,6 +6,7 @@ public class DayNightHandler : MonoBehaviour
 {
     [SerializeField] Material DaySkybox;
     [SerializeField] Material NightSkybox;
+    [SerializeField] Light Sun;
 
     // Update is called once per frame
     void Update()
@@ -13,10 +14,12 @@ public class DayNightHandler : MonoBehaviour
         if (Input.GetKeyDown("e"))
         {
             RenderSettings.skybox = DaySkybox;
+            Sun.enabled = true;
         }
         if (Input.GetKeyDown("q"))
         {
             RenderSettings.skybox = NightSkybox;
+            Sun.enabled = false;
         }
     }
 }
